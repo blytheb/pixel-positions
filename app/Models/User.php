0e4919814569b67609495a12d8f32637057aff80
blytheb <blytheballesteros@gmail.com>
2026-04-08 12:18:49 -1000

    edit factories for each model

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function employer()
+    {
+        return $this->hasOne(Employer::class);
+    }
 }
