2e453606482418c092288ece903fb0125208a173
blytheb <blytheballesteros@gmail.com>
2026-04-08 11:48:55 -1000

    build search banner and config tags

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,1 +1,17 @@
-<a href="#" class="bg-primary/20 hover:bg-primary/35 px-3 py-1 rounded-xl text-xs">{{ $slot}}</a>
+@props([
+    'size' => 'base'
+])
+
+@php
+    $classes= "bg-primary/20 hover:bg-primary/35 rounded-xl";
+
+    if($size ==='base'){
+        $classes .= " px-5 py-1 text-sm";
+    }
+
+    if ($size === 'small') {
+        $classes .= " px-3 py-1 text-xs";
+    }
+@endphp
+
+<a href="#" class="{{ $classes }}">{{ $slot}}</a>
